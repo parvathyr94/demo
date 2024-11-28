fa327854367b19a3d9e631bcac1c4135c1be25a6
parvathyr94 <parvathyr94@gmail.com>
2024-11-28 11:46:28 +0530

    initial files

---
+++ Chromelaunch.java
@@ -0,0 +1,13 @@
+package browswerlounching;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Chromelaunch {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+WebDriver driver=new ChromeDriver();
+	}
+
+}
